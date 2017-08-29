1926b3cfaa6c2206576820003a645f9429a0af03
bendalby82 <bdalby@pivotal.io>
2017-08-29 13:57:37 +0100

    Ready to push

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 
+# Edith goes to site page
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# She notices the page title and headr mention to-do lists
+assert 'To-Do' in browser.title
 
+
+browser.quit()
