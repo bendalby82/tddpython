ce3aaf29d7c1d89bce1b2bc06b5c889e15f8e5f2
bendalby82 <ben@dalby.net>
2017-09-01 17:29:10 +0100

    Interim commit

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,9 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
+#Better Unit Testing Practice: Each Test Should Test One Thing
+#http://chimera.labs.oreilly.com/books/1234000000754/ch05.html#_redirect_after_a_post
+
 class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page_view(self):
     found = resolve('/')
@@ -24,12 +27,17 @@ class HomePageTest(TestCase):
 
     response = home_page(request)
 
-    self.assertIn('A new list item', response.content.decode())
-    expected_html = render_to_string(
-      'home.html',
-      {'new_item_text': 'A new list item'}
-      )
-    self.assertEqual(response.content.decode(), expected_html)
+    self.assertEqual(Item.objects.count(),1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
+
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(response['location'], '/')
+
+  def test_home_page_only_saves_items_when_necessary(self):
+    request = HttpRequest()
+    home_page(request)
+    self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
 
