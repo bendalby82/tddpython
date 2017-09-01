62934eaabe50fe1c0b22d4553eebb45b4cd998d2
bendalby82 <ben@dalby.net>
2017-09-01 16:31:40 +0100

    Half way through adding new items

--- functional_tests.py
+++ functional_tests.py
@@ -32,18 +32,34 @@ class NewVisitorTest(unittest.TestCase):
     #When she hits enter, the page updates, and now the page lists
     # 1: Buy peacock feathers as an item in a to-do list table
     inputbox.send_keys(Keys.ENTER)
+    #import time
+    #time.sleep(30)
+
+    import time
+    time.sleep(5)
+
 
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
-    self.assertTrue(
-      any(row.text == '1: Buy peacock feathers' for row in rows), 
-      "New to-do item did not appear in table"
-      )
+    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
     #There is still a text box inviting her to another item. She
     #enters 'use peacock feathers to make a fly' (Edith is very 
     #methodical)
-    self.fail('Finish teh test!')
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Use peacock feathers to make a fly')   
+    inputbox.send_keys(Keys.ENTER)
+
+    import time
+    time.sleep(5)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+    self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+
+    self.fail('Finish the test!')
 
     #The page updates again, and now shows both items on her list
 
