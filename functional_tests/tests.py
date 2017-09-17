ec827855c8da017d10cb5d7d6ef9cee7755b19d7
bendalby82 <bdalby@pivotal.io>
2017-09-17 10:33:40 +0100

    New URL. view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -83,6 +83,8 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys('Buy milk')
     inputbox.send_keys(Keys.ENTER)
 
+    time.sleep(5)
+
     #Francis gets his own unique URL
     francis_list_url = self.browser.current_url
     self.assertRegex(francis_list_url, '/lists/.+')
